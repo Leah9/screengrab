c6ef4f891d303fb9c77aa6c50aebdfb2d7d59f9d
Leah C <101465290+Leah9@users.noreply.github.com>
2022-05-12 11:27:54 +0100

    Add comments. Do not show captured image by default

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Use to grab docs from Box
+#Use to grab docs from websites
 
 import pyscreenshot
 import pyautogui
@@ -6,18 +6,20 @@ import random
 import time
 import tkinter as tk
 
+#Define the App class
 class App:
-    def __init__(self, x1 = 0, y1 = 0, x2 = 640, y2 = 480):
+    def __init__(self, x1 = 0, y1 = 0, x2 = 640, y2 = 480): # Use sensible defaults
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
         self.image_no = 1001
+        self.show_image = False
 
     def get_number(self):
         return int(self.x1)
 
-    def get_box(self):
+    def get_box(self): # Returns the box dimensions in the correct format
         return (self.x1, self.y1, self.x2, self.y2)
 
     #def __repr__(self):
@@ -38,12 +40,11 @@ root.attributes('-topmost',True)
 #place a label on the root window
 message = tk.Label(root, text="Screencap")
 
+#Function is called when the Get Box button is clicked
 def get_box_button_clicked():
     print("Top left position in 5 seconds")
     time.sleep(5)
-    x1y1 = pyautogui.position()
-    #print(type(x1y1))
-    #print(x1y1.x)
+    x1y1 = pyautogui.position() # This gets the x and y position of the mouse the variable is an object
     print("Capturing bottom right in 5 seconds")
     time.sleep(5)
     x2y2 = pyautogui.position()
@@ -53,10 +54,12 @@ def get_box_button_clicked():
     capture.y2 = x2y2.y
     #print(pyautogui.position())
 
+#Called when the Capture button is clicked
 def button_clicked():
     print('Button clicked')
     image = pyscreenshot.grab(bbox=(capture.get_box()))
-    image.show()
+    if capture.show_image:
+        image.show()
     image.save(f"image{capture.image_no}.png")
     capture.image_no += 1
     print(capture.image_no)
