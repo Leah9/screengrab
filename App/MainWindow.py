ab6a42890b8b34a22839cc99f22637751f9dbc82
ArielMAJ <ariel.maj@hotmail.com>
2022-05-13 11:12:41 -0300

    Added message boxes to some actions. Added comments and docstrings (as suggested by pylint).
    Also renamed folders and files as suggested by pylint.

--- App/MainWindow.py
+++ App/MainWindow.py
@@ -1,96 +1,148 @@
-import tkinter as tk
-# import pyscreenshot
-import time
-from fpdf import FPDF
+"""This file is where the Class of the main window of the App is implemented."""
+
 import os
-from PIL import Image
+# import time
+import tkinter as tk
+from tkinter import messagebox
 import pyautogui
-from Globals import IMG_DIR
+from PIL import Image
+from fpdf import FPDF
+from globals import IMG_DIR
 
-
-# import Globals
 
 # Define the App class
 class MainWindow(tk.Tk):
+    """Main window"""
     def __init__(self, x1=0, y1=0, x2=640, y2=480):  # Use sensible defaults
         super().__init__()
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+        self.box = (x1, y1, x2, y2)
+        self.region = (x1, y1, x2 - x1, y2 - y1)
         self.image_no = 1001
         self.show_image = False
         self.pages = tk.IntVar()
         self.pages.set(5)
-        self.default_width = 20
-        self.default_pady = 5
-        self.default_border_width = 3
-        self.default_button_bg = "#DEDEDE"
-        self.draw_screen()
-        # self.region = self.x1, self.y1, self.x2-self.x1, self.y2-self.y1
+        self.default_params = {
+            "width": 20,
+            "pady": 5,
+            "borderwidth": 3,
+            "button_bg": "#DEDEDE",
+        }
+        self._draw_screen()
 
-    def draw_screen(self):
+    def _draw_screen(self):
+        """This function is inteded to be called when constructing this class (in __init__).
+        \rThis is where all the main widgets are supposed to be packed."""
+
         self.attributes('-topmost', True)  # Keeps GUI on top
         self.config(padx=20, pady=20)
-        # place a label on the root window
+        # Place a label on the root window
         message = tk.Label(self, text="ScreenGrab")
-
-        # Define the buttons
-        capture_button = tk.Button(self, text='Capture', borderwidth=self.default_border_width,
-                                   bg=self.default_button_bg, width=self.default_width, pady=self.default_pady,
-                                   command=lambda: self.capture_button_clicked())
-        # Pack the button, required.
-        capture_button.pack(pady=2)
-        get_box_button = tk.Button(self, text='Get Box', borderwidth=self.default_border_width,
-                                   bg=self.default_button_bg, width=self.default_width, pady=self.default_pady,
-                                   command=lambda: self.get_box_button_clicked())
-        get_box_button.pack(pady=2)
-        auto_button = tk.Button(self, text='Auto mode, down arrow', borderwidth=self.default_border_width,
-                                bg=self.default_button_bg, width=self.default_width, pady=self.default_pady,
-                                command=lambda: self.auto_button_clicked())
-        auto_button.pack(pady=2)
-        create_pdf_button = tk.Button(self, text='Create pdf', borderwidth=self.default_border_width,
-                                      bg=self.default_button_bg, width=self.default_width, pady=self.default_pady,
-                                      command=lambda: self.create_pdf_button_clicked())
-        create_pdf_button.pack(pady=2)
         message.pack(pady=2)
 
+        # Define the buttons
+        capture_button = tk.Button(
+            self,
+            text='Capture',
+            borderwidth=self.default_params["borderwidth"],
+            bg=self.default_params["button_bg"],
+            width=self.default_params["width"],
+            pady=self.default_params["pady"],
+            command=self.capture_button_clicked
+        )
+        # Pack the button, required.
+        capture_button.pack(pady=2)
+
+        get_box_button = tk.Button(
+            self,
+            text='Get Box', borderwidth=self.default_params["borderwidth"],
+            bg=self.default_params["button_bg"],
+            width=self.default_params["width"],
+            pady=self.default_params["pady"],
+            command=self.get_box_button_clicked)
+        get_box_button.pack(pady=2)
+
+        auto_button = tk.Button(
+            self,
+            text='Auto mode, down arrow',
+            borderwidth=self.default_params["borderwidth"],
+            bg=self.default_params["button_bg"],
+            width=self.default_params["width"],
+            pady=self.default_params["pady"],
+            command=self.auto_button_clicked)
+        auto_button.pack(pady=2)
+
+        create_pdf_button = tk.Button(
+            self,
+            text='Create pdf',
+            borderwidth=self.default_params["borderwidth"],
+            bg=self.default_params["button_bg"],
+            width=self.default_params["width"],
+            pady=self.default_params["pady"],
+            command=self.create_pdf_button_clicked)
+        create_pdf_button.pack(pady=2)
+
+
         # Text box for number of pages
-        pages_entry_box = tk.Entry(self, justify=tk.CENTER, borderwidth=self.default_border_width,
-                                   width=self.default_width, textvariable=self.pages)
+        pages_entry_box = tk.Entry(
+            self,
+            justify=tk.CENTER,
+            borderwidth=self.default_params["borderwidth"],
+            width=self.default_params["width"],
+            textvariable = self.pages)
         pages_entry_box.pack(pady=2)
 
     @property
     def number(self):
-        return int(self.x1)
+        """Top left x is returned as an integer"""
+        return int(self.box[0])
+
+    # @property
+    # def region(self):
+    #     """Property that returns a region."""
+    #     return (self.x1, self.y1, self.x2 - self.x1, self.y2 - self.y1)
+
 
     # @property
     # def box(self):  # Returns the box dimensions in the correct format
-    # return self.x1, self.y1, self.x2, self.y2
-
-    @property
-    def region(self):
-        return self.x1, self.y1, self.x2 - self.x1, self.y2 - self.y1
+    #     return self.x1, self.y1, self.x2, self.y2
 
     # Function is called when the Get Box button is clicked
     def get_box_button_clicked(self):
-        print("Top left position in 5 seconds")
-        time.sleep(5)
-        x1y1 = pyautogui.position()  # This gets the x and y position of the mouse the variable is an object
-        print("Capturing bottom right in 5 seconds")
-        time.sleep(5)
+        """This function will capture two points on the screen to create a bounding box."""
+        messagebox.showinfo(
+            title="Attention: TOP LEFT",
+            message="Position your mouse and press enter to capture TOP LEFT position.",
+            icon="question", # This specific icon removes the bell noise from the messagebox.
+            parent=self
+        )
+        # print("Top left position in 5 seconds")
+        # time.sleep(5)
+
+        # pyautogui.position() gets the x and y position of the mouse the variable is an object
+        x1y1 = pyautogui.position()
+
+        messagebox.showinfo(
+            title="Attention: BOTTOM RIGHT",
+            message="Position your mouse and press enter to capture BOTTOM RIGHT position",
+            icon="question",
+            parent=self
+        )
+        # print("Capturing bottom right in 5 seconds")
+        # time.sleep(5)
+
         x2y2 = pyautogui.position()
-        print("Captured area")
+
         # update the values in the object
-        self.x1 = x1y1.x
-        self.y1 = x1y1.y
-        self.x2 = x2y2.x
-        self.y2 = x2y2.y
+        self.box = (*x1y1, *x2y2)
+        messagebox.showinfo(title="Finished", message=f"Captured area was {self.box}", parent=self)
+
+        # print("Captured area")
         # print(self.box)
         # print(pyautogui.position())
 
     # Called when the Capture button is clicked
     def capture_button_clicked(self):
+        """This will capture an screen shot of the bounding box area when called."""
         print('Button clicked')
         image = pyautogui.screenshot(region=self.region)
         # image = pyscreenshot.grab(bbox=(self.box))
@@ -101,17 +153,25 @@ class MainWindow(tk.Tk):
         print(self.image_no)
 
     def auto_button_clicked(self):
+        """This will "auto click the capture button" (or, rather, call its function) for a
+        \rgiven amount of times. That amount is written in the entry box."""
         self.show_image = False
-        print("Starting auto capture in 5 seconds")
-        time.sleep(5)
-        for i in range(self.pages.get()):
+        messagebox.showinfo(
+            title="Attention",
+            message="Press enter to start capturing the screen.",
+            parent=self
+        )
+        # print("Starting auto capture in 5 seconds")
+        # time.sleep(5)
+        for _ in range(self.pages.get()):
             self.capture_button_clicked()
             pyautogui.press('down')
             print(f"Captured image {self.image_no}")
 
     def create_pdf_button_clicked(self):
+        """This function will concatenate all images in the IMG_DIR folder into a PDF."""
         print("Getting list of images from img folder")
-        images_list = [x for x in os.listdir(IMG_DIR)]
+        images_list = os.listdir(IMG_DIR)
         print(images_list)
         pdf = FPDF('l', 'pt', 'A4')  # Init pdf l = landscape, pt = points / pixels, A4 default size
         pdf.set_auto_page_break(True)
@@ -121,7 +181,8 @@ class MainWindow(tk.Tk):
             print(timage.width, timage.height)
             # Below will add a page the same size as the image.
             pdf.add_page(format=(timage.height, timage.width))
-            # format=(timage.height, timage.width)) # format keyword gives out an error if using fpdf, fpdf2 is required
+            # format keyword gives out an error if using fpdf, fpdf2 is required
             # pip uninstall fpdf, pip install fpdf2
             pdf.image(f"{IMG_DIR}/{img}")
         pdf.output("Binder.pdf")
+        messagebox.showinfo(title="Success", message="Finished creating PDF.", parent=self)
--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-# Use to grab docs or images from screen one page at a time
+"""Main file of the ScreenGrab tool.
+Use to grab docs or images from screen one page at a time.
+"""
 
-import pyautogui
 import os
-import App
-from Globals import IMG_DIR
-# import Globals
+import pyautogui
+import app
+from globals import IMG_DIR
 
 
 def main():
+    """Main function"""
+
     # Checking if directory exists.
     if not os.path.exists(IMG_DIR):
         # Creates it in case it doesn't
         os.makedirs(IMG_DIR)
 
     # Init Tkinter call our GUI root
-    root = App.MainWindow(408, 146, 1683, 1037)
+    root = app.MainWindow(408, 146, 1683, 1037)
 
     print("get box :" + str(root.region))
     pyautogui.FAILSAFE = True
