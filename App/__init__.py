a6805b0f143e5634e108275b355439c0dbf9332f
ArielMAJ <ariel.maj@hotmail.com>
2022-05-13 08:15:21 -0300

    Added message boxes to some actions. Added comments and docstrings (as suggested by pylint).
    Also renamed folders and files and suggested by pylint as well.

--- App/__init__.py
+++ App/__init__.py
@@ -1,1 +1,19 @@
-from .MainWindow import MainWindow
+"""This file is so Python identifies this folder as a package.
+This should import all app windows."""
+
+# The imports below exist so we can import as:
+# import app
+# root = app.MainWindow()
+
+# instead of :
+# from app import MainWindow
+# root = main_window.MainWindow()
+
+# or
+
+# import app
+# root = app.main_window.MainWindow()
+
+# The dot before the file name means python should try to import a file in this same folder.
+# from .file import SomeClass, SomeVariable, SomeFunction, ...
+from .main_window import MainWindow
--- Globals/__init__.py
+++ Globals/__init__.py
@@ -1,1 +1,11 @@
-from .settings import *
+"""This file is so Python identifies this folder as a package."""
+
+# The imports below exist so we can import as:
+# from Globals import IMG_DIR
+
+# instead of :
+# from Globals.settings import IMG_DIR
+
+# The dot before the file name means python should try to import a file in this same folder.
+# from .file import SomeClass, SomeVariable, SomeFunction, ...
+from .settings import * # "*" means import everything in there.
