330b8eaac2d4e13191c39a8e377dd588b3b1dd5a
ArielMAJ <ariel.maj@hotmail.com>
2022-05-15 19:32:48 -0300

    Tests should now be run with pytest
    Changed workflow so it installs and runs pytest as well

--- tests/test_main_window.py
+++ tests/test_main_window.py
@@ -1,17 +1,25 @@
 """
 This module implements assertions tests on the main window. When run,
 they should raise AssertionErrors if something isn't right with the code.
-If all assertions pas the package should print "PASSED".
 
-Read this package's __main__.py docstring for guidance on how to run it.
+To run this you'll need pytest installed. Then make sure to run pytest from
+this repository's root folder.
+
+Example on how to run pytest (this may vary on each machine):
+pytest
+python -m pytest
+py -m pytest
 """
 
 import os
 from PIL import Image
 import App
 
+if not os.path.exists("img"):
+    os.makedirs("img")
 
-def capture_size_full_screen():
+
+def test_capture_size_full_screen():
     """
     This will assert capture_button_clicked captures images of the
     correct expected size: (1920, 1080)
@@ -36,7 +44,7 @@ def capture_size_full_screen():
     assert image.size == (1920, 1080)
 
 
-def capture_size_small_box():
+def test_capture_size_small_box():
     """
     This will assert capture_button_clicked captures images of the
     correct expected size: (200, 200).
